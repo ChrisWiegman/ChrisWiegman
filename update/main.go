666f8d8c6bd5164168714e9201ca286d9a4881ad
Chris Wiegman <chris@chriswiegman.com>
2023-02-01 15:29:00 -0500

    Fix link in Bio

--- update/main.go
+++ update/main.go
@@ -47,7 +47,7 @@ func main() {
 	line2 := "I am an **Engineering Manager** at **[WP Engine](https://wpengine.com/)**, devoted to improving the developer experience for WordPress developers, particularly those working on headless projects. My work, both for my day job and side projects, focuses on the intersection of development, privacy, ethics and usability of software and development to help improve the lives of everyone who uses modern technology."
 	line3 := "My latest blog post is: **[" + latestPost.Title + "](" + latestPost.Link + ")**. "
 	line4 := "Like this type of content? Take a look at my site, **[chriswiegman.com](https://chriswiegman.com/)** and subscribe via its **[RSS feed](https://chriswiegman.com/feed/)**."
-	line5 := "You can find me on **[Mastodon](https://mastodon.chriswiegman.com/@chris)** and **[Pixelfed](https:/pixelfed.chriswiegman.com)** or view **[my full resume](https://gist.github.com/ChrisWiegman/8a89d7c2aca775884ae4227ca3b5be01)**."
+	line5 := "You can find me on **[Mastodon](https://mastodon.chriswiegman.com/@chris)** and **[Pixelfed](https:/pixelfed.chriswiegman.com/@chris)** or view **[my full resume](https://gist.github.com/ChrisWiegman/8a89d7c2aca775884ae4227ca3b5be01)**."
 	line6 := "<sub>Last updated: " + time.Now().Format("January 2, 2006") + "</sub>"
 
 	content := fmt.Sprintf("%s\n\n%s\n\n%s\n\n%s%s\n\n%s\n\n%s\n", line1, line2, line3, postLine, line4, line5, line6)
